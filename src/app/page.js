9ececb4eb1965c8446e4cfeb3703b11ded83e330
abdurrashidkhan <rashidkhan.destop@gmail.com>
2024-12-20 09:53:46 +0600

    change font and anather section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import BenefitsOfUse from "@/components/BenefitsOfUse/BenefitsOfUse";
 import MainPage from "@/components/MainPage/MainPage";
 import Navbar from "@/components/Navbar/Navbar";
 
@@ -6,6 +7,12 @@ export default function Home() {
     <>
       <Navbar />
       <MainPage />
+      <BenefitsOfUse />
+      {/* কিভাবে কাজ করবে আমাদের এই ঔষধ  */}
+      {/* এই ওষুধে কি কি উপকার আপনার হবে  */}
+      {/* এই ওষুধে কি কি অউপকার আপনার হবে  */}
+      {/* ওষুধে সম্পর্কে  অভিমত */}
+      {/* সচরাচর জানতে চাওয়া প্রশ্নের উত্তর */}
     </>
   );
 }
---
+++ src/components/BenefitsOfUse/BenefitsOfUse.jsx
@@ -0,0 +1,18 @@
+export default function BenefitsOfUse() {
+  return (
+    <section>
+      <div className="container mx-auto px-2 py-10">
+        <div className="text-center ">
+          <h1 className="text-3xl font-medium">
+            {" "}
+            <strong className="text-4xl font-semibold text-slate-800">
+              স্যাফরন
+            </strong>{" "}
+            ব্যবহারের উপকারিতা{" "}
+          </h1>
+          <p className="pt-3 pb-6">এই ঔষধ ব্যবহারে কি কি উপকারিতা পাবেন |</p>
+        </div>
+      </div>
+    </section>
+  );
+}
